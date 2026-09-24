ce330eefdb601f543c6f310415f8df98f2a6ae4d
Yevhenii Hladyshenko <135309229+evgeniiiiiii@users.noreply.github.com>
2026-09-24 19:03:40 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,25 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from bot.config import BOT_TOKEN
+from bot.database import init_db
+from bot.handlers import start, user, admin
 
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    await init_db()
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_router(start.router)
+    dp.include_router(admin.router)
+    dp.include_router(user.router)
+
+    print("Бот запущен...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+  
