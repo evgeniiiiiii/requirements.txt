5c3cfaf2e7b0306e0acfdd20fddb1ea32bcd8760
Yevhenii Hladyshenko <135309229+evgeniiiiiii@users.noreply.github.com>
2026-09-24 19:00:54 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+
+raw_url = os.getenv("DATABASE_URL", "")
+DATABASE_URL = raw_url.replace("postgresql://", "postgresql+asyncpg://")
