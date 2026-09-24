006c641ee3239650e7a174621092754e3f4ae3f3
Yevhenii Hladyshenko <135309229+evgeniiiiiii@users.noreply.github.com>
2026-09-24 19:01:45 +0200

    Add state classes for bot categories and items

--- bot/states.py
+++ bot/states.py
@@ -1,1 +1,23 @@
+from aiogram.fsm.state import State, StatesGroup
 
+class AddCategory(StatesGroup):
+    name = State()
+
+class AddItem(StatesGroup):
+    category = State()
+    title = State()
+    description = State()
+    content_type = State()
+    media = State()
+
+class EditItem(StatesGroup):
+    title = State()
+    description = State()
+    content_type = State()
+    media = State()
+
+class Search(StatesGroup):
+    query = State()
+
+class DeleteCategory(StatesGroup):
+    choose = State()
